21108032303db74b3786d281ebbdda01fdd8307e
Charles <cao510927@gmail.com>
2022-05-02 16:23:19 +0800

    Delete R_shell directory

--- R_shell/get_promoter.R
+++
@@ -1,8 +0,0 @@
-library("TxDb.Hsapiens.UCSC.hg38.knownGene")
-
-setwd("/data5/galaxy/project/data/promoter")
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-genes_txdb <- genes(txdb)
-promoters_txdb <- promoters(genes_txdb)
-#By default, the promoters function will fetch the 2000 nucleotides before the transcription start site (TSS) and the 200 nucleotides after the TSS.
-promoters_txdb
